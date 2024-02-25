332c8992722ed0c119c6c7c4cd48bb25c68213dc
GyeongChan-Jang <cham9994@gmail.com>
2024-02-25 21:26:49 +0900

    config: base model entity 생성

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,12 +1,9 @@
 import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
 import {UsersModel} from "../../users/entities/users.entity";
+import {BaseModel} from "../../common/entity/base.entity";
 
 @Entity()
-export class PostsModel {
-  // 자동으로 생성되는 id 필드를 정의
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class PostsModel extends BaseModel {
   // 1) UsersModel과 연동한다 Foreign Key를 이용해서
   // 2) null이 될 수 없다.
   @ManyToOne(() => UsersModel, (user) => user.posts, {
